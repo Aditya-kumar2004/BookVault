b06883030e741c26aa989dea15dc0b70fc982f18
Aditya Kumar <adityakuma876@gmail.com>
2026-05-25 02:28:21 +0530

    feat: run migrations and seeding automatically on Docker startup

--- backend/bookStoreBackend/docker-entrypoint.sh
+++ backend/bookStoreBackend/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Run database migrations and seeders automatically
+php artisan migrate --force
+php artisan db:seed --force
+
 # Cache configurations for speed in production
 php artisan config:cache
 php artisan route:cache
